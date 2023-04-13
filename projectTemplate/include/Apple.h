528025dc3493c69d6aab64a2e467939f201e301f
e <manon2012@126.com>
2023-04-13 11:54:44 +0800

    Add files via upload
    code template

---
+++ projectTemplate/include/Apple.h
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef APPLE
+#define APPLE
+#include "Fruit.h"
+
+class Apple : public Fruit
+{
+ public:
+   void show();
+
+
+};
+
+
+#endif // !APPLE
+
+
+
+
---
+++ projectTemplate/src/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include<string.h>
+#include "../include/Apple.h"
+using namespace std;
+
+
+
+/*
+   modify
+   1. .vscode/launch.json  
+   "program": "${workspaceFolder}/build/template2023",
+   2.CmakeLists.txt
+   add_executable(Template2023 src/main.cpp) #添加生成可执行文件
+   target_link_libraries(template2023 pthread)
+        
+*/
+
+int main()
+{
+
+   cout<<"hello,world! tempalte2013"<<endl;
+
+   return 0;
+
+
+}   
