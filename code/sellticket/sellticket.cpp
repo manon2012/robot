1a2f2a45f1e63c79f318827e34a815c08bf12613
wupei <manon2012@126.com>
2023-04-22 21:08:58 +0800

    with versions

--- code/sellticket/sellticket.cpp
+++ code/sellticket/sellticket.cpp
@@ -1,5 +1,5 @@
 // sellticket.cpp : This file contains the 'main' function. Program execution begins and ends there.
-// 
+//
 
 #include <iostream>
 #include <thread>
@@ -9,25 +9,124 @@ using namespace std;
 mutex mtx;
 int gcount = 10;
 
-void sellticket(int index)
+void sellticket1(int index)
 {
-    //mtx.lock(); //1 if before while, only sellticket0 do it. lock while 
+    mtx.lock();
     while (gcount > 0)
-    {
-        mtx.lock(); //2
-        if (gcount > 0)
+    { 
+		cout << "sellticket" << index << ":" << gcount << endl;
+		gcount--;
+       
+    }
+    mtx.unlock();
+    this_thread::sleep_for(chrono::microseconds(10));
+}
+
+void sellticket2(int index)
+{
+	while (gcount > 0)
+	{
+        mtx.lock();
+		cout << "sellticket" << index << ":" << gcount << endl;
+		gcount--;
+        mtx.unlock();
+		
+	}
+    this_thread::sleep_for(chrono::microseconds(10));
+
+}
+
+void sellticket3(int index)
+{
+	while (gcount > 0)
+	{
+        mtx.lock();
+        if (gcount>0)
         {
 			cout << "sellticket" << index << ":" << gcount << endl;
 			gcount--;
         }
-      
-        mtx.unlock(); //2
-    }
+        mtx.unlock();
+	}
+	
+    //it's none of your business
+    //this_thread::sleep_for(chrono::microseconds(10));
+
+}
+
+void sellticket4(int index)
+{
+	while (gcount > 0)
+	{
+		mtx.lock();
+		if (gcount > 0)
+		{
+			cout << "sellticket" << index << ":" << gcount << endl;
+			gcount--;
+		}
+		mtx.unlock();
+        this_thread::sleep_for(chrono::microseconds(10));
+	}
+}
+
+void sellticket5(int index)
+{
+	while (gcount > 0)
+	{
+		lock_guard<mutex> lock(mtx); 
+		if (gcount > 0)
+		{
+			cout << "sellticket" << index << ":" << gcount << endl;
+			gcount--;
+		}
+		this_thread::sleep_for(chrono::microseconds(10));
+	}
+}
+
+void sellticket6(int index)
+{
+	while (gcount > 0)
+	{
+		{    //不能用在函数参数传递和返回过程中  只能用在简单的临界区代码互斥
+			lock_guard<mutex> lock(mtx);  //lock_gurad没有拷贝构造 复制重载
+			if (gcount > 0)
+			{
+				cout << "sellticket" << index << ":" << gcount << endl;
+				gcount--;
+			}
+		}
+		this_thread::sleep_for(chrono::microseconds(10));
+	}
+	
+}
 
 
-    //mtx.unlock();  //1
-    this_thread::sleep_for(chrono::microseconds(10));
+void sellticket7(int index)
+{
+	while (gcount > 0)
+	{
+		{   
+			unique_lock<mutex> lck(mtx);//右值得拷贝 
+			if (gcount > 0)
+			{
+				cout << "sellticket" << index << ":" << gcount << endl;
+				gcount--;
+			}
+		}
+		this_thread::sleep_for(chrono::microseconds(10));
+	}
 
+	/*
+	  unique_lock<mutex> lck(mtx);   //可用于函数调用过程中 线程通信
+	  cv.wait(lck) 1使线程进入等待状态 2 lck.unlock可以把mtx释放掉
+	*/
+
+
+	/*
+	vc.notify_all()
+	通知在cv上等待的线程，条件成立了，起来干活了~
+	其他在cv上等待的线程，收到通知，从等待状态->阻塞状态->获取互斥锁了->线程继续往下执行
+	*/
 }
 
 int main()
@@ -35,7 +134,7 @@ int main()
     list<thread> tlist;
     for (int i=0;i<3; i++)
     {
-        tlist.push_back(thread(sellticket, i));
+        tlist.push_back(thread(sellticket6, i));
     }
 
     for (auto& t : tlist)
@@ -60,16 +159,16 @@ int main()
 
 //1 lock before while
 /*
-sellticket0:10
-sellticket0:9
-sellticket0:8
-sellticket0:7
-sellticket0:6
-sellticket0:5
-sellticket0:4
-sellticket0:3
-sellticket0:2
-sellticket0:1
+sellticket1:10
+sellticket1:9
+sellticket1:8
+sellticket1:7
+sellticket1:6
+sellticket1:5
+sellticket1:4
+sellticket1:3
+sellticket1:2
+sellticket1:1
 Hello World!
 */
 
@@ -89,4 +188,51 @@ sellticket0 : 1
 sellticket2 : 0
 sellticket1 : -1
 Hello World!
+*/
+
+
+//t3
+/*
+ellticket0:10
+sellticket0:9
+sellticket0:8
+sellticket0:7
+sellticket0:6
+sellticket0:5
+sellticket0:4
+sellticket0:3
+sellticket0:2
+sellticket0:1
+Hello World!
+*/
+
+//t4
+/*
+sellticket0:10
+sellticket1:9
+sellticket2:8
+sellticket0:7
+sellticket2:6
+sellticket1:5
+sellticket1:4
+sellticket0:3
+sellticket2:2
+sellticket1:1
+Hello World!
+
+*/
+
+
+//t5
+/*
+sellticket0:10
+sellticket0:9
+sellticket0:8
+sellticket0:7
+sellticket0:6
+sellticket0:5
+sellticket0:4
+sellticket0:3
+sellticket0:2
+sellticket0:1
 */
