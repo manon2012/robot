25df2709de148f94d5feb55998b775113ed3ac20
manon2012 <manon2012@126.com>
2023-04-24 18:36:32 +0800

    complex version1

---
+++ code/projectComplex/src/main.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <string.h>
+#include "../include/Apple.h"
+using namespace std;
+
+/*
+   modify
+   1. .vscode/launch.json
+   "program": "${workspaceFolder}/build/template2023",
+   2.CmakeLists.txt
+   add_executable(Template2023 src/main.cpp) #添加生成可执行文件
+   target_link_libraries(template2023 pthread)
+
+*/
+
+class Complex
+{
+   friend ostream &operator<<(ostream &cout, const Complex &other);
+
+public:
+   Complex(double x = 0, double y = 0) : m_real(x), m_imag(y)
+   {
+   }
+
+   Complex(const Complex &other)
+   {
+      this->m_real = other.m_real;
+      this->m_imag = other.m_imag;
+   }
+
+   Complex operator=(const Complex &other)
+   {
+      this->m_real = other.m_real;
+      this->m_imag = other.m_imag;
+   }
+
+   Complex &operator+=(const Complex &other)
+   {
+      this->m_real += other.m_real;
+      this->m_imag += other.m_imag;
+      return *this;
+   }
+
+   ~Complex()
+   {
+   }
+
+private:
+   double m_real;
+   double m_imag;
+};
+ostream &operator<<(ostream &cout, const Complex &other)
+{
+   cout << "other.real: " << other.m_real << ",other.imag: " << other.m_imag << std::endl;
+   return cout;
+}
+
+void test01()
+{
+   Complex a;
+   Complex b(1.0, 2.0);
+   Complex c(b);
+   Complex d;
+   d = c;
+   std::cout << a << std::endl;
+   std::cout << b << std::endl;
+   std::cout << c << std::endl;
+   std::cout << d << std::endl;
+   cout << "test01" << endl;
+}
+
+int main()
+{
+   test01();
+   cout << "hello,world! tempalte2013" << endl;
+
+   return 0;
+}
