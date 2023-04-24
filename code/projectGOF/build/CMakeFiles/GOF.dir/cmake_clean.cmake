a046fbc9a436188a5af39a82842586665ecc19ae
manon2012 <manon2012@126.com>
2023-04-24 08:59:26 +0800

    add GOF

---
+++ code/projectGOF/build/CMakeFiles/GOF.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GOF.dir/src/main.cpp.o"
+  "GOF.pdb"
+  "GOF"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GOF.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
