f26ad7d0a0f10ade184eea9d461c2fd26676bd5c
manon2012 <manon2012@126.com>
2023-05-09 17:50:39 +0800

    commit again

--- PycharmProjects/pythonProject/main.py
+++ PycharmProjects/pythonProject/main.py
@@ -1,4 +1,7 @@
 # This is a sample Python script.
+import fnmatch
+import os
+
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -12,8 +15,24 @@ def print_hi(name):
     print(a)
     print(b)
 
+def findmp3():
+    rootPath = '/'
+    pattern = '*.mp3'
+
+    for root, dirs, files in os.walk(rootPath):
+        for filename in fnmatch.filter(files, pattern):
+            print(os.path.join(root, filename))
+
+
+def test01():
+    sum = 0
+    for i in range(1,101):
+        sum += i
+    print (sum)
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    #print_hi('PyCharm')
+    #findmp3()
+    test01()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
